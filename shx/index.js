ef2401040f27081e35954443b42f6029013968b0
Sean Nolan <sean@thenolans.com>
2023-06-04 11:23:08 -0700

    qr

--- shx/index.js
+++ shx/index.js
@@ -1,7 +1,19 @@
+//
+// Read about this code at http://shutdownhook.com
+// MIT license details at https://github.com/seanno/shutdownhook/blob/main/LICENSE
+//
+
 const process = require('process');
 const jose = require('node-jose');
+const fs = require('fs');
+const zlib = require('zlib');
+const qrcode = require('qrcode');
 
-const makeKeys = async () => {
+// +---------+
+// | newKeys |
+// +---------+
+
+const newKeys = async () => {
 
   const keystore = jose.JWK.createKeyStore();
   const props = { "use": "sig", "alg": "ES256" };
@@ -14,8 +26,63 @@ const makeKeys = async () => {
   console.log(">>> PRIVATE\n" + JSON.stringify(privateJSON, null, 2));
 }
 
+// +----------+
+// | signCard |
+// +----------+
+
+const signCard = async (cardPath, keyStorePath) => {
+
+  const cardJSON = JSON.parse(fs.readFileSync(cardPath));
+  const compressedCard = zlib.deflateRawSync(JSON.stringify(cardJSON));
+
+  const keystoreJSON = JSON.parse(fs.readFileSync(keyStorePath));
+  const keystore = await jose.JWK.asKeyStore(keystoreJSON);
+  const signingKey = keystore.all()[0];
+
+  const options = { format: 'compact', fields: { zip: 'DEF' } };
+  const signer = jose.JWS.createSign(options, signingKey);
+  const jws = await signer.update(Buffer.from(compressedCard)).final();
+
+  console.log(jws);
+}
+
+// +--------+
+// | MakeQR |
+// +--------+
+
+const makeQR = async (jws, path) => {
+  
+  const numericJWS = jws.split('')
+        .map((c) => c.charCodeAt(0) - 45)
+        .flatMap((c) => [Math.floor(c / 10), c % 10]) // Need to maintain leading zeros
+        .join('');
+
+  const segments = [
+	{ data: 'shc:/', mode: 'byte' },
+	{ data: numericJWS, mode: 'numeric' }
+  ];
+
+  await qrcode.toFile(path, segments, {
+	width: 600,
+	errorCorrectionLevel: 'L'
+  });
+}
+
+// +------------+
+// | entrypoint |
+// +------------+
+
+const usage = () => {
+  console.log("USAGE:");
+  console.log("  node . newkeys");
+  console.log("  node . signCard JSON_PATH KEYSTORE_PATH");
+  console.log("  node . makeQR JWS QR_PATH");
+}
+
 switch (process.argv[2]) {
-  case "newkey": makeKeys(); break;
-  default: console.log("USAGE: node . [newkey]");
+  case "newkeys": newKeys(); break;
+  case "signCard": signCard(process.argv[3], process.argv[4]); break;
+  case "makeQR": makeQR(process.argv[3], process.argv[4]); break;
+  default: usage(); break;
 }
 
