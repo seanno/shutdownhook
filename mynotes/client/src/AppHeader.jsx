fa763d976708de517cb2e1bd9701725ef9012e0d
Sean Nolan <sean@thenolans.com>
2024-11-08 16:59:45 -0800

    about button

--- mynotes/client/src/AppHeader.jsx
+++ mynotes/client/src/AppHeader.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { IconButton } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 export default function AppHeader({ fhir }) {
 
@@ -12,12 +13,21 @@ export default function AppHeader({ fhir }) {
 	  <br/><br/>
 	  
 	  { fhir &&
-		<a href="/">
-		  <IconButton aria-label="logout">
-			<LogoutIcon style={{ transform: 'scaleX(-1)' }} />
-		  </IconButton>
-		</a>
+		<>
+		  <a href="/">
+			<IconButton aria-label="logout">
+			  <LogoutIcon style={{ transform: 'scaleX(-1)' }} />
+			</IconButton>
+		  </a>
+		  <br/>
+		</>
 	  }
+
+	  <a target="_blank" href="https://shutdownhook.com/explain-my-notes/">
+		<IconButton aria-label="about">
+		  <HelpOutlineIcon />
+		</IconButton>
+	  </a>
     </>
   )
 }
